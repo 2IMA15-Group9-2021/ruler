911bf3e29d4755eeb8470039e080e2f31447a373
Rick Wosyka <rickwosyka@gmail.com>
2021-12-15 11:45:08 +0100

    Added *triangles*

--- unity/Assets/Stealth/Objects/GalleryCamera.cs
+++ unity/Assets/Stealth/Objects/GalleryCamera.cs
@@ -18,6 +18,9 @@ namespace Stealth
         [SerializeField]
         private float gizmoLength = 1f;
 
+        private MeshFilter mf;
+        private Mesh tri;
+
         /// <summary>
         /// Backing field for <see cref="FieldOfViewDegrees"/>.
         /// </summary>
@@ -53,6 +56,45 @@ namespace Stealth
                 _fieldOfView = value;
                 FieldOfViewDegrees = Mathf.Rad2Deg * value;
             }
+        }
+
+        void Start()
+        {
+            mf = GetComponent<MeshFilter>();
+            tri = mf.mesh;
+            tri.vertices = new Vector3[] { Vector3.zero, Vector3.zero, Vector3.zero };
+            tri.triangles = new int[] { 0, 1, 2 };
+            mf.mesh = tri;
+        }
+
+
+        private Mesh generateTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+        {
+            tri = mf.mesh;
+            tri.Clear();
+
+            tri.vertices = new Vector3[] { v1, v2, v3 };
+            //mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
+            tri.triangles = new int[] { 0, 1, 2 };
+
+            mf.mesh = tri;
+
+            return tri;
+        }
+
+        void OnValidate()
+        {
+            if (mf == null)
+            {
+                mf = GetComponent<MeshFilter>();
+            }
+
+            Quaternion rot1 = Quaternion.Euler(0, 0, 0.5f * FieldOfViewDegrees);
+            Quaternion rot2 = Quaternion.Euler(0, 0, -0.5f * FieldOfViewDegrees);
+            Mesh mesh = generateTriangle(Vector3.zero,
+                rot1 * new Vector3( 1, 0, 0 ) * gizmoLength,
+                rot2 * new Vector3( 1, 0, 0 ) * gizmoLength);
+            Graphics.DrawMeshNow(mesh, transform.position, Quaternion.Euler(0, 0, 0));
         }
 
         /// <summary>
